49bdecd4e7ca98b9be628e3f5c0baab8f75c4c78
Hitesh Yadav <hiteshyadav3607@gmail.com>
2024-10-16 15:36:28 +0530

    / route added

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
 
 app.get('/users', (req, res) => {
     const html = `
