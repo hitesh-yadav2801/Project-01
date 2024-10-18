b8ce98623e5f7f137ea649d393cebd08894b70fc
Hitesh Yadav <hiteshyadav3607@gmail.com>
2024-10-18 14:49:34 +0530

    middleware learned

--- index.js
+++ index.js
@@ -1,86 +1,108 @@
-const express = require('express');
-const fs = require('fs');
-const users = require('./MOCK_DATA.json');
-const cors = require('cors');
+const express = require("express");
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors(
-    {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        credentials: true,
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.ip} ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
     }
-));
+  );
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.use((req, res, next) => {
+  console.log("Hello from middleware 2");
+  next();
+});
 
-app.get('/users', (req, res) => {
-    const html = `
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
-    ${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
+    ${users
+      .map((user) => `<li>${user.first_name} ${user.last_name}</li>`)
+      .join("")}
     </ul>
-    `
-    res.send(html); 
-})
+    `;
+  res.send(html);
+});
 
 // REST API
-app.get('/api/users', (req, res) => {
-    return res.json(users);
-})
+app.get("/api/users", (req, res) => {
+  res.setHeader("X-Author", "Hitesh Yadav");
+  return res.json(users);
+});
 
-app.route('/api/users/:id')
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find(user => user.id == id);
-        return res.json(user);
-    })
-    .patch((req, res) => {
-        const id = Number(req.params.id);
-        const body = req.body;
-        const userIndex = users.findIndex(user => user.id == id);
-        console.log(userIndex)
-        users[userIndex] = {
-            ...users[userIndex],
-            ...body,
-        };
-        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-            return res.json({status: 'success'});
-        })
-    })
-    .delete((req, res) => {
-        const id = Number(req.params.id);
-        const userIndex = users.findIndex(user => user.id == id);
-        users.splice(userIndex, 1);
-        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-            return res.json({status: 'success'});
-        })
-    });
-
-app.get('/api/users/:id', (req, res) => {
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find(user => user.id == id);
+    const user = users.find((user) => user.id == id);
     return res.json(user);
-})
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex((user) => user.id == id);
+    console.log(userIndex);
+    users[userIndex] = {
+      ...users[userIndex],
+      ...body,
+    };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "success" });
+    });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id == id);
+    users.splice(userIndex, 1);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "success" });
+    });
+  });
 
-app.post('/api/users', (req, res) => {
-    const body = req.body
-    users.push({
-        id: users.length + 1,
-        first_name: body.first_name,
-        last_name: body.last_name,
-        email: body.email,
-        gender: body.gender
-    })
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.json({status: 'success', id: users.length });
-    })
-})
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id == id);
+  return res.json(user);
+});
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  users.push({
+    id: users.length + 1,
+    first_name: body.first_name,
+    last_name: body.last_name,
+    email: body.email,
+    gender: body.gender,
+  });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.status(201).json({ status: "success", id: users.length });
+  });
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-console.log('')
+console.log("");
